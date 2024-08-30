75fe30c0db94dafa1ee09a291147f986cdcc7abe
Rakha Ramadhana <rakha.ramadhana@yahoo.com>
2024-08-30 22:20:06 +0800

    Update functions.php to use .env

--- functions.php
+++ functions.php
@@ -1,12 +1,15 @@
 <?php
-$servername = "localhost";
 
-$username = "root";
-$password = "";
-$database = "dst_db";
+require_once __DIR__ . '/vendor/autoload.php';
 
-//koneksi ke database
-//$conn = mysqli_connect("localhost", "root", "", "storyone");
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = $_ENV['DB_SERVER'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$database = $_ENV['DB_DATABASE'];
+
 $conn = mysqli_connect($servername, $username, $password, $database);
 
 // function ini menerima query dari halaman lain
